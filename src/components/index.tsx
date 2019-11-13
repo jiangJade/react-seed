452ab2950663937d651f635f900ba530ea0604d9
jiangJade <359813946@qq.com>
2019-11-13 14:15:04 +0800

    添加路由 解决一些报错

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -4,83 +4,60 @@ import {
     HashRouter as Router,
     Link
 } from 'react-router-dom'
-import {  Icon,Menu  } from 'antd';
+import { Icon, Menu } from 'antd';
 import "./home.scss";
 // import Heads from "./header/Header";
+import Page1 from './pages/Page1';
 
 const SubMenu = Menu.SubMenu;
 
-interface IState {
-    rootSubmenuKeys: any[],
-    openKeys: string[],
-    openKeys01s: any[]
-}
-
-interface handleIndex {
+interface HandleIndex {
     key: string,
     keyPath: any[]
 }
 
-interface initOpentChange {
-    openKeys: string[],
-    find: (key: string) => string
-}
+class Index extends React.Component {
 
-class Homepage extends React.Component<IState> {
-
-    public state: IState = {
+    public state = {
         rootSubmenuKeys : ['sub1', 'sub2',"sub3", 'sub4','sub5','sub6','sub7','sub8'],
         openKeys: ['sub1'],
         openKeys01s: ['1']
     }
-    // constructor(props) {
-    //     super(props);
-    //     this.state = {
-           
-    //     }
-    // }
+ 
+    // 环境变量
     public componentDidMount() {
-        //环境变量
         this.webRefresh();
     }
 
-    // emitter事件跳转更新menu
-    public handleCall = (v: IState) => {
+    // 导航点击
+    public checNative = (index: HandleIndex) => {
+        const opkeys = [index.key];
         this.setState({
-            openKeys01s: v.openKeys01s,
-            openKeys: v.openKeys
-        });
-    };
-
-    //导航点击
-    public checNative = (index: handleIndex) => {
-        let opkeys = [index.key];
-        this.setState({
-            openKeys01s: opkeys,
+            openKeys01s: opkeys
         });
         sessionStorage.setItem("InNative", JSON.stringify(index.keyPath[0]));
         sessionStorage.setItem("OutNative", JSON.stringify(index.keyPath[1]));
     };
 
-    //网页刷新
-    public webRefresh = () => {
-        let OutNative = sessionStorage.getItem("OutNative");
-        let InNative = sessionStorage.getItem("InNative");
-        if(OutNative === null || InNative===null){
+    // 网页刷新
+    public webRefresh = (): any => {
+        const OutNative = sessionStorage.getItem("OutNative");
+        const InNative = sessionStorage.getItem("InNative");
+        if(OutNative === null || InNative===null) {
             return false;
         }
         this.setState({
-            openKeys:[OutNative],
-            openKeys01s:[InNative]
+            openKeys: [OutNative],
+            openKeys01s: [InNative]
         })
     }
 
-    public returnChangeKey = (key: string) => {
+    public returnChangeKey = (key: any) => {
         return this.state.openKeys.indexOf(key) === -1;
     }
     
-    public onOpenChange = (openKeys: initOpentChange) => {
-        const latestOpenKey = openKeys.find((key: string) => this.returnChangeKey(key));
+    public onOpenChange = (openKeys: string[]) => {
+        const latestOpenKey: any = openKeys.find((key: any) => this.returnChangeKey(key));
         if (this.state.rootSubmenuKeys.indexOf(latestOpenKey) === -1) {
             this.setState({ openKeys });
         } else {
@@ -95,13 +72,13 @@ class Homepage extends React.Component<IState> {
             <div className="home_div">
                 {/* <Heads/> */}
                 <Router>
-                    {/* 系统导航 */}
+                    { /* 系统导航 */ }
                     <div className="sys_navigation">
-                        {/* 左侧导航部分 */}
+                        { /* 左侧导航部分 */ }
                         <div className="sys_navigatLeft" id="Glob_Leftnav">
                             <div className="sys_text_nav">
                                 <Menu
-                                    onClick={this.checNative.bind(this)}
+                                    onClick={(index) => this.checNative(index)}
                                     // defaultSelectedKeys={["menu0001"]}
                                     // defaultOpenKeys={["sub1"]}
                                     openKeys={this.state.openKeys}
@@ -115,21 +92,22 @@ class Homepage extends React.Component<IState> {
                                     </SubMenu>
                                     <SubMenu key="sub2" title={<span><Icon type="alert" /><span>交通监控</span></span>}>
                                         <Menu.Item key="4"><Link to='/home/EventHandle'>事件处置管理</Link></Menu.Item>
-                                        <Menu.Item key="5"><Link to='/home/EventTypes' replace>事件源管理</Link></Menu.Item>
-                                        <Menu.Item key="6"><Link to='/home/EventPlane' replace>处置方案管理</Link></Menu.Item>
+                                        <Menu.Item key="5"><Link to='/home/EventTypes'>事件源管理</Link></Menu.Item>
+                                        <Menu.Item key="6"><Link to='/home/EventPlane'>处置方案管理</Link></Menu.Item>
                                     </SubMenu>
                                     <SubMenu key="sub3" title={<span><Icon type="file-done" /><span>养护管理</span></span>}>
-                                        <Menu.Item key="7"><Link to='/home/CuringCalendar' replace>养护日历</Link></Menu.Item>
-                                        <Menu.Item key="8"><Link to='/home/CuringReportForm' replace>养护报表</Link></Menu.Item>
-                                        <Menu.Item key="10"><Link to='/home/DiseaseCollect' replace>病害汇总</Link></Menu.Item>
+                                        <Menu.Item key="7"><Link to='/home/CuringCalendar'>养护日历</Link></Menu.Item>
+                                        <Menu.Item key="8"><Link to='/home/CuringReportForm'>养护报表</Link></Menu.Item>
+                                        <Menu.Item key="10"><Link to='/home/DiseaseCollect'>病害汇总</Link></Menu.Item>
                                     </SubMenu>
                                 </Menu>
                             </div>
                         </div>
 
-                        {/* 右侧内容部分 */}
+                        { /* 右侧内容部分 */ }
                         <div className="sys_navigatRight" id="Glob_Rightnav">
-                            {this.props.children}
+                            {/* {this.props.children} */}
+                            <Page1 />
                         </div>
                     </div>
                 </Router>
@@ -139,4 +117,4 @@ class Homepage extends React.Component<IState> {
 
 }
 
-export default Homepage;
+export default Index;
--- src/components/pages/Page1.tsx
+++ src/components/pages/Page1.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react'
-import { }
 import './index.scss';
 
 const Page1 = () => (
